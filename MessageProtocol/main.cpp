b19d52b6cd4a7aa596fa30051b6757c575773394
hspark <hyunsoo_park2@tmax.co.kr>
2018-01-29 12:56:10 +0900

    let's serialize

--- MessageProtocol/main.cpp
+++ MessageProtocol/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+
+
+
 #include "DummyClasses.h"
+
+
+//
+//class Serializable {
+//public:
+//    Serializable(){}
+//    virtual ~Serializable(){}
+//
+//    virtual void serialize(std::ostream& stream);
+//    virtual void deserialize(std::istream& stream);
+//};
+
 
 int main() {
     std::string a = "hello world0101010";
@@ -13,8 +28,30 @@ int main() {
 
     dca.sendsTo(&dca2);
 
+    ////////////////////serialization test
+
+
+
     return 0;
 }
 
 //would this be needed?
 ////appendsTo(SendableObject)
+//
+//int main()
+//{
+//    using namespace mydata;
+//    MyData data { "this is a name", "this is a type", boost::make_shared<MyInfo>("this is info") };
+//
+//    std::ostringstream oss;
+//    {
+//        boost::archive::text_oarchive oa(oss);
+//        oa << data;
+//    }
+//
+//    std::istringstream iss(oss.str());
+//    {
+//        boost::archive::text_iarchive ia(iss);
+//        ia >> data;
+//    }
+//}
